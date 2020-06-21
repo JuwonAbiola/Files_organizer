e0c2a32c48567e09ccd5dbaaea4ca0435dd769fe
jhayX <lordsylharx@gmail.com>
2020-06-21 13:11:19 +0100

    made the home directory the default directory

--- app.py
+++ app.py
@@ -1,25 +1,32 @@
 import shutil
 import os
+from os.path import expanduser
 
-source = '../'
+home = expanduser("~")
+os.chdir(home)
+current_folder = 'Files_organizer'
+source = 'Downloads'
 files = os.listdir(source)
 
-### Mac OS users only
+print(source)
+
+
+# Mac OS users only
 
 def transfer(src, doc):
-    location = src.get(ext, '../../Documents')
-    return shutil.move(f"{source}{doc}", location) if not os.path.exists(f"{location}/{doc}") \
+    location = src.get(ext, 'Documents')
+    return shutil.move(f"{source}/{doc}", location) if not os.path.exists(f"{location}/{doc}") \
         else print(f'{doc} already exist in {location}')
 
 
 for file in files:
     switcher = {
-        '.mp3': '../../Music',
-        '.mp4': '../../Movies',
-        '.png': '../../Pictures',
-        '.jpg': '../../Pictures',
-        '.jpeg': '../../Pictures',
+        '.mp3': 'Music',
+        '.mp4': 'Movies',
+        '.png': 'Pictures',
+        '.jpg': 'Pictures',
+        '.jpeg': 'Pictures',
     }
     ext = os.path.splitext(file)[1]
-    if file != 'Files_organizer':
+    if file != current_folder:
         transfer(switcher, file)
